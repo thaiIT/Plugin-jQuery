1bce27a4fcf542bfba539d1f1da277cf1f39b3e6
thaiIT <hoangminhthai94@gmail.com>
2019-05-20 07:16:00 +0700

    Script for tabs

---
+++ script-for-tabs.php
@@ -0,0 +1,48 @@
+<ul class="faq">
+	<li class="item1">
+		<a href="#" title="click here" class="active">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor vehicula ipsum nec ?</a>
+		<ul>
+			<li class="subitem1">
+				<p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore.
+					At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque
+				corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+			</li>
+		</ul>
+	</li>
+	<li class="item2">
+		<a href="#" title="click here" class="">The standard Lorem Ipsum passage Etiam faucibus viverra libero vel efficitur. Ut semper nisl ut laoreet ultrices ?</a>
+		<ul>
+			<li class="subitem1">
+				<p> Tincidunt ut laoreet dolore At vero eos et Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
+					nibh euismod consectetuer adipiscing elit, sed diam nonummy nibh euismod accusamus et iusto odio dignissimos ducimus
+					qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
+				cupiditate non provident.</p>
+			</li>
+		</ul>
+	</li>
+</ul>
+
+<!-- script for tabs -->
+	<script>
+		$(function () {
+
+			var menu_ul = $('.faq > li > ul'),
+				menu_a = $('.faq > li > a');
+
+			menu_ul.hide();
+
+			menu_a.click(function (e) {
+				e.preventDefault();
+				if (!$(this).hasClass('active')) {
+					menu_a.removeClass('active');
+					menu_ul.filter(':visible').slideUp('normal');
+					$(this).addClass('active').next().stop(true, true).slideDown('normal');
+				} else {
+					$(this).removeClass('active');
+					$(this).next().stop(true, true).slideUp('normal');
+				}
+			});
+
+		});
+	</script>
+	<!-- script for tabs -->
